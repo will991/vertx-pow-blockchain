74605ebfb1cbc2e6a718c7f5bf82de618d3788a9
William Wolff <wolff.willi@gmail.com>
2022-08-19 10:45:08 +0200

    [Verticles] Added main verticle and blockchain sync verticle in sequential deployment

---
+++ src/main/java/io/chain/MainVerticle.java
@@ -0,0 +1,44 @@
+package io.chain;
+
+import io.chain.models.Blockchain;
+import io.chain.verticles.BlockchainSyncVerticle;
+import io.chain.verticles.RestApiVerticle;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+
+import static java.lang.String.format;
+
+public class MainVerticle extends AbstractVerticle {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+
+    private Blockchain blockchain = new Blockchain();
+
+    @Override
+    public void start(Promise<Void> startPromise) {
+        deploy(new RestApiVerticle(blockchain), new DeploymentOptions())
+            .compose(r -> deploy(new BlockchainSyncVerticle(blockchain), new DeploymentOptions()))
+            .onSuccess(startPromise::complete)
+            .onFailure(startPromise::fail);
+    }
+
+    private <T extends AbstractVerticle> Future<Void> deploy(T verticle, DeploymentOptions opts) {
+        LOGGER.info("Deploying " + verticle.getClass().getSimpleName());
+        Promise<Void> promise = Promise.promise();
+        vertx
+            .deployVerticle(verticle, opts)
+            .onSuccess(result -> {
+                LOGGER.info(format("Successfully deployed: %s", verticle.getClass().getSimpleName()));
+                promise.complete();
+            })
+            .onFailure(err -> {
+                LOGGER.error(format("Failed deploying: %s", verticle.getClass().getSimpleName()), err);
+                promise.fail(err);
+            });
+        return promise.future();
+    }
+
+}
---
+++ src/main/java/io/chain/p2p/handlers/BlockchainSyncHandler.java
@@ -0,0 +1,42 @@
+package io.chain.p2p.handlers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.chain.models.Block;
+import io.chain.models.Blockchain;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static java.lang.String.format;
+import static java.lang.System.lineSeparator;
+
+@Getter
+@RequiredArgsConstructor
+public final class BlockchainSyncHandler implements Handler<Buffer> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BlockchainSyncHandler.class);
+
+    private final String uuid;
+    private final Blockchain blockchain;
+
+    @Override
+    public void handle(Buffer buffer) {
+        final ObjectMapper mapper = new ObjectMapper();
+        try {
+            List<Block> blocks = mapper.readValue(buffer.toJsonArray().encode(), new TypeReference<>(){});
+            Blockchain newBlockchain = new Blockchain(blocks);
+
+            if (blockchain.replace(newBlockchain)) {
+                LOGGER.info(format("[%s] Replacing chain with new one:%s%s", uuid, lineSeparator(), newBlockchain.toJson().encodePrettily()));
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+}
