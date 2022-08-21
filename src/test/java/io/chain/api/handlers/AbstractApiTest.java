b0101966c5d555683fc2cf6b33f4da29d9d661ec
William Wolff <wolff.willi@gmail.com>
2022-08-21 22:12:17 +0200

    [TEST] Added helper methods for RestApi testing

--- src/test/java/io/chain/api/handlers/AbstractApiTest.java
+++ src/test/java/io/chain/api/handlers/AbstractApiTest.java
@@ -3,10 +3,16 @@ package io.chain.api.handlers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.chain.MainVerticle;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import lombok.AccessLevel;
@@ -14,12 +20,11 @@ import lombok.Getter;
 import lombok.Setter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
 
 @ExtendWith(VertxExtension.class)
 abstract class AbstractApiTest {
     protected final static Logger LOGGER = LoggerFactory.getLogger(AbstractApiTest.class);
+    private final static int DEFAULT_TEST_PORT = 9001;
 
     @Getter(AccessLevel.PROTECTED)
     @Setter(AccessLevel.PRIVATE)
@@ -33,11 +38,39 @@ abstract class AbstractApiTest {
         client = WebClient.create(vertx);
     }
 
+    protected final Future<WebClient> launchClient(Vertx vertx, int port) {
+        final Promise<WebClient> promise = Promise.promise();
+
+        final JsonObject httpConfig = new JsonObject()
+                .put("host", "localhost")
+                .put("port", port);
+        final JsonObject config = new JsonObject().put("http", httpConfig);
+
+        vertx
+            .deployVerticle(MainVerticle.class, new DeploymentOptions().setConfig(config))
+            .onSuccess(r -> promise.complete(
+                WebClient.create(
+                    vertx,
+                    new WebClientOptions().setDefaultHost("localhost").setDefaultPort(port)
+                )
+            ))
+            .onFailure(promise::fail);
+        return promise.future();
+    }
+
     protected final HttpRequest<Buffer> get(String requestURI) {
-        return client.get(8080, "localhost", requestURI);
+        return get(DEFAULT_TEST_PORT, requestURI, client);
+    }
+
+    protected final HttpRequest<Buffer> get(int port, String requestURI, WebClient client) {
+        return client.get(port, "localhost", requestURI);
     }
 
     protected final HttpRequest<Buffer> post(String requestURI) {
-        return client.post(8080, "localhost", requestURI);
+        return post(DEFAULT_TEST_PORT, requestURI, client);
+    }
+
+    protected final HttpRequest<Buffer> post(int port, String requestURI, WebClient client) {
+        return client.post(port, "localhost", requestURI);
     }
 }
