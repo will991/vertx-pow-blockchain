94aa0d1fbe9727ab7e4252dd149fe4ccb0534f00
William Wolff <wolff.willi@gmail.com>
2022-08-22 17:49:30 +0200

    [VERTICLE] Added additional testing class for ease of verticle testing

--- src/test/java/io/chain/api/handlers/AbstractApiTest.java
+++ src/test/java/io/chain/api/handlers/AbstractApiTest.java
@@ -2,41 +2,21 @@ package io.chain.api.handlers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.chain.MainVerticle;
+import io.chain.verticles.AbstractVerticleTest;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
-import io.vertx.core.impl.logging.Logger;
-import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
-import io.vertx.junit5.VertxExtension;
-import io.vertx.junit5.VertxTestContext;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(VertxExtension.class)
-abstract class AbstractApiTest {
-    protected final static Logger LOGGER = LoggerFactory.getLogger(AbstractApiTest.class);
+abstract class AbstractApiTest extends AbstractVerticleTest {
     private final static int DEFAULT_TEST_PORT = 9001;
 
-    @Getter(AccessLevel.PROTECTED)
-    @Setter(AccessLevel.PRIVATE)
-    protected static WebClient client;
-
     protected final ObjectMapper mapper = new ObjectMapper();
-
-    @BeforeEach
-    final void setup(Vertx vertx, VertxTestContext context) {
-        vertx.deployVerticle(MainVerticle.class, new DeploymentOptions(), result -> context.completeNow());
-        client = WebClient.create(vertx);
-    }
 
     protected final Future<WebClient> launchClient(Vertx vertx, int port) {
         final Promise<WebClient> promise = Promise.promise();
---
+++ src/test/java/io/chain/verticles/AbstractVerticleTest.java
@@ -0,0 +1,50 @@
+package io.chain.verticles;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.chain.MainVerticle;
+import io.chain.models.UTxOSet;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import lombok.AccessLevel;
+import lombok.Setter;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(VertxExtension.class)
+public abstract class AbstractVerticleTest {
+
+    @Setter(AccessLevel.PRIVATE)
+    protected WebClient client;
+
+    protected UTxOSet configureInitialUTxOSet() {
+        return new UTxOSet();
+    }
+
+    @BeforeEach
+    private void setup(Vertx vertx, VertxTestContext context) throws JsonProcessingException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final JsonObject utxos = new JsonObject()
+                .put("initialUTxOs", new JsonArray(mapper.writeValueAsString(configureInitialUTxOSet().getUtxos().values())));
+        vertx.deployVerticle(
+            MainVerticle.class,
+            new DeploymentOptions().setConfig(utxos),
+            result -> context.completeNow()
+        );
+        client = WebClient.create(vertx);
+    }
+
+    @AfterEach
+    private void cleanUp(Vertx vertx, VertxTestContext context) {
+        if (client != null) {
+            client.close().onSuccess(v -> context.completeNow()).onFailure(context::failNow);
+        }
+    }
+
+}
