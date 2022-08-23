b4e9383ceb49350ad52bd53568f009a1e46f4c9c
William Wolff <wolff.willi@gmail.com>
2022-08-23 19:13:52 +0200

    [HANDLER] Added newly mined block handler to update global utxo set and miner wallet

--- src/main/java/io/chain/p2p/handlers/NewBlockHandler.java
+++ src/main/java/io/chain/p2p/handlers/NewBlockHandler.java
@@ -7,52 +7,43 @@ import io.chain.models.Transaction;
 import io.chain.models.UTxOSet;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.Message;
-import io.vertx.core.json.Json;
-import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import lombok.RequiredArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static io.chain.p2p.handlers.NewUnconfirmedTransactionHandler.UNCONFIRMED_TX_POOL;
-import static java.lang.String.format;
 
 @RequiredArgsConstructor
-public final class NewBlockHandler implements Handler<Message<JsonArray>> {
+public final class NewBlockHandler implements Handler<Message<JsonObject>> {
 
     private final UTxOSet utxoSet;
     private final Vertx vertx;
 
     @Override
-    public void handle(Message<JsonArray> message) {
-        final JsonObject blockJson = message.body().getJsonObject(message.body().size() - 1);
-        final Block lastBlock;
+    public void handle(Message<JsonObject> message) {
         try {
-            lastBlock = new ObjectMapper().readValue(blockJson.encode(), Block.class);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace(System.err);
-            message.reply("Could not deserialize block data.");
-            return;
-        }
-
-        try {
-            final JsonObject blockData = Buffer.buffer(lastBlock.getData()).toJsonObject();
-            final List<Transaction> blockTxs = new ArrayList<>();
-
-            for (final Object raw: blockData.getJsonArray("txs")) {
-                final JsonObject rawTx = (JsonObject) raw;
-                blockTxs.add(Json.decodeValue(rawTx.toBuffer(), Transaction.class));
+            final JsonObject blockJson = message.body();
+            final Block lastBlock;
+            try {
+                lastBlock = new ObjectMapper().readValue(blockJson.encode(), Block.class);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace(System.err);
+                message.fail(400, "Could not deserialize block data.");
+                return;
             }
 
+            final List<Transaction> blockTxs = lastBlock.getTransactions();
             final List<Transaction> processedTxs = utxoSet.process(blockTxs);
-            processedTxs.forEach(tx ->
-                vertx.sharedData().getLocalMap(UNCONFIRMED_TX_POOL).remove(tx.hash())
-            );
+
+            processedTxs
+                .forEach(tx ->
+                    vertx.sharedData().getLocalMap(UNCONFIRMED_TX_POOL).remove(tx.hash())
+                );
+            message.reply("");
         } catch (Exception e) {
-            message.reply(format("Could not deserialize block data for: %s", lastBlock.getHash()));
+            e.printStackTrace(System.err);
         }
     }
 }
