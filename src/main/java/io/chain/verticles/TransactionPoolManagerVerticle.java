0efe3cdcf48511b50c3018c67ccda807cc538a35
William Wolff <wolff.willi@gmail.com>
2022-08-23 10:48:16 +0200

    [VERTICLE] Simplified chain sync handler

--- src/main/java/io/chain/verticles/TransactionPoolManagerVerticle.java
+++ src/main/java/io/chain/verticles/TransactionPoolManagerVerticle.java
@@ -3,39 +3,27 @@ package io.chain.verticles;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.chain.models.Transaction;
 import io.chain.models.UTxO;
 import io.chain.models.UTxOSet;
+import io.chain.p2p.handlers.UnconfirmedTransactionHandler;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
-import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 import static io.chain.p2p.EventBusAddresses.NEW_TRANSACTION;
-import static java.lang.String.format;
 
 @RequiredArgsConstructor
 public final class TransactionPoolManagerVerticle extends AbstractEventBusVerticle {
 
-    public static final String UNCONFIRMED_TX_POOL = "io.chain.unconfirmed.txs.pool";
-    private final String uuid;
     private final UTxOSet utxos;
+    private final String uuid;
 
     @Override
     public void start(Promise<Void> startPromise) {
-        this.<String>register(NEW_TRANSACTION, msg -> {
-            final Transaction tx = Json.decodeValue(msg.body(), Transaction.class);
-            if (Transaction.isValid(tx, utxos)) {
-                LOGGER.info(format("[%s] Added transaction to pool", uuid));
-                vertx
-                    .sharedData()
-                    .<String, Transaction>getLocalMap(UNCONFIRMED_TX_POOL)
-                    .put(tx.hash(), tx);
-            }
-        });
+        register(NEW_TRANSACTION, new UnconfirmedTransactionHandler(uuid, utxos, vertx));
 
         initUTxOSet()
             .onSuccess(startPromise::complete)
