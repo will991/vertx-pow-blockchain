2bdfc899ec4472feffbb3a4d07248cc20ab40e8c
William Wolff <wolff.willi@gmail.com>
2022-08-23 10:49:00 +0200

    [EXCEPTION] Added toJson for Transaction validation exceptions for rest apis

--- src/main/java/io/chain/models/exceptions/TransactionValidationException.java
+++ src/main/java/io/chain/models/exceptions/TransactionValidationException.java
@@ -1,11 +1,16 @@
 package io.chain.models.exceptions;
 
 import io.chain.models.Transaction;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 
 import static java.lang.String.format;
 import static java.lang.System.lineSeparator;
 
 public abstract class TransactionValidationException extends Exception {
+
+    private Transaction tx;
+    private String msg;
 
     public TransactionValidationException(Transaction tx, String message) {
         super(
@@ -15,5 +20,11 @@ public abstract class TransactionValidationException extends Exception {
                     tx
             )
         );
+        this.tx = tx;
+        this.msg = message;
+    }
+
+    public JsonObject toJson() {
+        return new JsonObject().put("error", format("Transaction validation failed. %s", msg));
     }
 }
