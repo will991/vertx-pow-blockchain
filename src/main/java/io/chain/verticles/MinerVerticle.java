4b988754f1a646d523b93b058b16c9a0cb2aaa6a
William Wolff <wolff.willi@gmail.com>
2022-08-24 15:00:35 +0200

    [CLEAN] Clean up miner verticle + indention

--- src/main/java/io/chain/verticles/MinerVerticle.java
+++ src/main/java/io/chain/verticles/MinerVerticle.java
@@ -3,14 +3,15 @@ package io.chain.verticles;
 import io.chain.models.*;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 import lombok.RequiredArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static io.chain.LocalMapConstants.UNCONFIRMED_TX_POOL;
 import static io.chain.p2p.EventBusAddresses.NEW_BLOCKCHAIN;
-import static io.chain.p2p.handlers.NewUnconfirmedTransactionHandler.UNCONFIRMED_TX_POOL;
 
 @RequiredArgsConstructor
 public final class MinerVerticle extends AbstractVerticle {
@@ -29,9 +30,13 @@ public final class MinerVerticle extends AbstractVerticle {
          * (3) Tell p2p to sync chains and include new block with those transactions
          * (4) Every other node now needs to remove txs from their mem pool since they are now confirmed
          */
-        final List<Transaction> unconfirmedTxs = new ArrayList<>(
-            vertx.sharedData().<String, Transaction>getLocalMap(UNCONFIRMED_TX_POOL).values()
-        );
+        final List<Transaction> unconfirmedTxs =
+            vertx
+                .sharedData()
+                .<String, JsonObject>getLocalMap(UNCONFIRMED_TX_POOL)
+                .values()
+                .stream()
+                .map(t -> Json.decodeValue(t.toBuffer(), Transaction.class)).collect(Collectors.toList());
         unconfirmedTxs.add(Transaction.rewardTransaction(wallet));
 
         vertx.
