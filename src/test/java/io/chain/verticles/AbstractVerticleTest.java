bc2a59a834f3348febf2168e5e20ed6796a2c29f
William Wolff <wolff.willi@gmail.com>
2022-08-24 15:00:50 +0200

    [TEST] Fixed import for local map identifier

--- src/test/java/io/chain/verticles/AbstractVerticleTest.java
+++ src/test/java/io/chain/verticles/AbstractVerticleTest.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import static io.chain.p2p.handlers.NewUnconfirmedTransactionHandler.UNCONFIRMED_TX_POOL;
+import static io.chain.LocalMapConstants.UNCONFIRMED_TX_POOL;
 
 @ExtendWith(VertxExtension.class)
 public abstract class AbstractVerticleTest {
